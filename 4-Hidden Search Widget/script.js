98e31db3d7e630e2979bfbfd9b2479be36ecf45a
jy19950924 <350965504@qq.com>
2022-07-04 23:10:39 +0800

    finished the project named the Hidden search widget

--- 4-Hidden Search Widget/script.js
+++ 4-Hidden Search Widget/script.js
@@ -1,9 +1,8 @@
 let searchBtn = document.querySelector('i')
 let inputBox = document.querySelector('.search')
 
-// console.log(inputEle)
 searchBtn.addEventListener('click', function () {
-    document.getElementById('searchTxt').focus();
+    document.querySelector('input').focus();
     inputBox.classList.toggle('active');
 }, false);
 
