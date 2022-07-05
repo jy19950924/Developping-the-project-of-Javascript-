9cb2b9e8d8e76e17c59718e1e868357936c1766c
jy19950924 <350965504@qq.com>
2022-07-06 00:34:20 +0800

    增加book lib中html,css,js相关逻辑

--- 4-2 Book lib/script.js
+++ 4-2 Book lib/script.js
@@ -1,48 +1,77 @@
 
 let addButton = document.getElementById('add');
-let deleteButton = document.getElementById('delete');
+let checkButton = document.getElementById('check');
 let table = document.querySelector('table');
 
+let checkHTML = '<i id="delete" class="fa-solid fa-circle-minus"></i>'
+let checkNum = 0;
+const booksUrl = 'http://www.liulongbin.top:3006';
 
-
-function getBooksList() {
-    const booksUrl = 'http://www.liulongbin.top:3006';
+function getBookList() {
     let getBooks = new XMLHttpRequest();
     getBooks.open('GET', `${booksUrl}/api/getbooks`);
     getBooks.send();
     getBooks.onreadystatechange = function () {
         if (getBooks.readyState === 4 && getBooks.status === 200) {
             const books = JSON.parse(getBooks.responseText).data;
-            // console.log(books)
             books.forEach(book => {
-                // console.log(book)
                 table.insertRow();
                 let len = table.rows.length;
-                for (let i = 0; i < dataRange; i++) {
+                Object.values(book).forEach((value, index) => {
                     table.rows[len - 1].insertCell();
-                    table.rows[len - 1].cells[i].innerText = Object.values(book)[i];
-                }
+                    table.rows[len - 1].cells[index].innerText = value;
+                });
+                table.rows[len - 1].insertCell();
+                const lastCell = table.rows[len - 1].cells.length;
+                table.rows[len - 1].cells[lastCell - 1].innerHTML = checkHTML;
             });
         }
     }
 }
-getBooksList();
 
-const dataRange = 4;
+getBookList();
 
 addButton.addEventListener('click', function () {
-
-    console.log(table.insertRow());
+    console.log('html')
+    table.insertRow();
     let len = table.rows.length;
-    for (let i = 0; i < dataRange; i++) {
-        table.rows[len - 1].insertCell();
-        table.rows[len-1 ].insertCell
+    let keyNum = table.rows[0].cells.length;
+    for (let i = 0; i < keyNum; i++) {
+        table.rows[len - 1].insertCell()
+        table.rows[len - 1].cells[i].contentEditable = true;
+        if (i === 0) {
+            table.rows[len - 1].cells[i].innerText = len;
+        }
     }
-    console.log(table.rows[len - 1].cells[0].contentEditable)
 
+    checkButton.style.display = 'inline';
 }, false);
 
-deleteButton.addEventListener('click', function () {
-    console.log("delete");
-}, false);
+// deleteButton.addEventListener('click', function () {
+//     console.log()
+//     console.log("delete");
 
+// }, false);
+
+checkButton.addEventListener('click', function () {
+    const len = table.rows.length;
+    const bookInfo = table.rows[len - 1].cells;
+    const bookName = bookInfo[1].innerText;
+    const author = bookInfo[2].innerText;
+    const publisher = bookInfo[3].innerText;
+    console.log('bookInfo', bookInfo)
+    console.log('bookName', bookName)
+    console.log('author', author)
+    console.log('publisher', publisher)
+    let addBook = new XMLHttpRequest();
+    addBook.open('POST', `${booksUrl}/api/addbook`);
+    addBook.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    addBook.send(`bookname=${bookName}&author=${author}&publisher=${publisher}`);
+    console.log(addBook)
+    addBook.onreadystatechange = function () {
+        if (addBook.readyState === 4 && addBook.status === 200) {
+            console.log(addBook.responseText)
+            checkButton.style.display = "none";
+        }
+    }
+}, false);
