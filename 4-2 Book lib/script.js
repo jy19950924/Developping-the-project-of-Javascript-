7a73dd1a43c229bdb5a9bd55fa14544340e4dac2
coderSheep <64951456+jy19950924@users.noreply.github.com>
2022-07-06 18:31:33 +0800

    Update script.js

--- 4-2 Book lib/script.js
+++ 4-2 Book lib/script.js
@@ -3,7 +3,7 @@ let addButton = document.getElementById('add');
 let checkButton = document.getElementById('check');
 let table = document.querySelector('table');
 
-let checkHTML = '<i id="delete" class="fa-solid fa-circle-minus"></i>'
+let deleteHTML = '<i style=":hover{}" class="fa-solid fa-circle-minus"></i>'
 let checkNum = 0;
 const booksUrl = 'http://www.liulongbin.top:3006';
 
@@ -14,64 +14,79 @@ function getBookList() {
     getBooks.onreadystatechange = function () {
         if (getBooks.readyState === 4 && getBooks.status === 200) {
             const books = JSON.parse(getBooks.responseText).data;
-            books.forEach(book => {
-                table.insertRow();
-                let len = table.rows.length;
+            books.forEach((book) => {
+                let curRow = table.insertRow();
                 Object.values(book).forEach((value, index) => {
-                    table.rows[len - 1].insertCell();
-                    table.rows[len - 1].cells[index].innerText = value;
+                    let cell = curRow.insertCell();
+                    cell.innerText = value;
                 });
-                table.rows[len - 1].insertCell();
-                const lastCell = table.rows[len - 1].cells.length;
-                table.rows[len - 1].cells[lastCell - 1].innerHTML = checkHTML;
+                let deleteCell = curRow.insertCell();
+                deleteCell.innerHTML = deleteHTML;
+
+                //注册删除函数
+                deleteCell.addEventListener('click', function () {
+                    let deleteBook = new XMLHttpRequest();
+                    let id = book.id;
+                    deleteBook.open('GET', `${booksUrl}/api/delbook?id=${id}`);
+                    deleteBook.send()
+                    deleteBook.onreadystatechange = function () {
+                        if (deleteBook.readyState === 4 && deleteBook.status === 200) {
+                            console.log(deleteBook.responseText);
+                            const curRowArray = Array.from(curRow.cells);
+                            const bookIndex = Array.from(table.rows).findIndex((row) => {
+                                return Array.from(row.cells).every((cell, index) => {
+                                    return cell.innerText === curRowArray[index].innerText;
+                                });
+                            })
+                            table.deleteRow(bookIndex);
+                        }
+                    }
+                }, false);
+
             });
         }
     }
 }
 
-getBookList();
+
 
 addButton.addEventListener('click', function () {
-    console.log('html')
-    table.insertRow();
+    let row = table.insertRow();
     let len = table.rows.length;
     let keyNum = table.rows[0].cells.length;
     for (let i = 0; i < keyNum; i++) {
-        table.rows[len - 1].insertCell()
-        table.rows[len - 1].cells[i].contentEditable = true;
-        if (i === 0) {
-            table.rows[len - 1].cells[i].innerText = len;
-        }
+        let cell = row.insertCell()
+        cell.contentEditable = true;
     }
 
     checkButton.style.display = 'inline';
-}, false);
-
-// deleteButton.addEventListener('click', function () {
-//     console.log()
-//     console.log("delete");
-
-// }, false);
-
-checkButton.addEventListener('click', function () {
-    const len = table.rows.length;
-    const bookInfo = table.rows[len - 1].cells;
-    const bookName = bookInfo[1].innerText;
-    const author = bookInfo[2].innerText;
-    const publisher = bookInfo[3].innerText;
-    console.log('bookInfo', bookInfo)
-    console.log('bookName', bookName)
-    console.log('author', author)
-    console.log('publisher', publisher)
-    let addBook = new XMLHttpRequest();
-    addBook.open('POST', `${booksUrl}/api/addbook`);
-    addBook.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-    addBook.send(`bookname=${bookName}&author=${author}&publisher=${publisher}`);
-    console.log(addBook)
-    addBook.onreadystatechange = function () {
-        if (addBook.readyState === 4 && addBook.status === 200) {
-            console.log(addBook.responseText)
-            checkButton.style.display = "none";
+    addButton.disabled = true;
+    checkButton.addEventListener('click', function () {
+        const bookInfo = row.cells;
+        const bookName = bookInfo[1].innerText;
+        const author = bookInfo[2].innerText;
+        const publisher = bookInfo[3].innerText;
+        let addBook = new XMLHttpRequest();
+        addBook.open('POST', `${booksUrl}/api/addbook`);
+        addBook.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        addBook.send(`bookname=${bookName}&author=${author}&publisher=${publisher}`);
+        console.log(addBook)
+        addBook.onreadystatechange = function () {
+            if (addBook.readyState === 4 && addBook.status === 200) {
+                console.log(addBook.responseText)
+                checkButton.style.display = "none";
+                let curLen = row.cells.length;
+                let deleteCell = row.cells[curLen - 1];
+                deleteCell.innerHTML = deleteHTML;
+                // clear property of the editable
+                row.cells.forEach((cell) => {
+                    cell.contentEditable = false;
+                })
+            }
         }
-    }
+    }, false);
 }, false);
+
+getBookList();
+
+
